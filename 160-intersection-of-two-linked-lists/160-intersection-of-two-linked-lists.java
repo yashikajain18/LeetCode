e8a4cf3cdc9bfff2fdcc772e13548e0b7be34994
yashikajain18 <95409412+yashikajain18@users.noreply.github.com>
2022-08-27 00:46:07 +0530

    Time: 2 ms (59.39%), Space: 55.3 MB (58.36%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -10,16 +10,43 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA, b = headB;
-    
-        while(a!=b){
-          if(a==null) a = headB;
-          else a = a.next;
-        
-          if(b==null) b = headA;
-          else b = b.next;
+    public static int length(ListNode head){
+        int len=0;
+        while(head!=null){
+            head=head.next;
+            len++;
         }
-        return b;
+        return len;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       int lenA=length(headA);
+       int lenB=length(headB);
+       ListNode temp1=null;
+       temp1= (lenA>lenB) ? headA:headB;
+        
+       ListNode temp2=null;
+       temp2=(lenA>lenB) ? headB:headA; 
+      
+       for(int i=0; i<Math.abs(lenA-lenB); i++){
+           temp1=temp1.next;
+       }
+        while(temp1!=null){
+            if(temp1==temp2) return temp1;
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        return null;
+        
+        //Best Approach
+//         ListNode a = headA, b = headB;
+    
+//         while(a!=b){
+//           if(a==null) a = headB;
+//           else a = a.next;
+        
+//           if(b==null) b = headA;
+//           else b = b.next;
+//         }
+        //return b;
     }
 }
