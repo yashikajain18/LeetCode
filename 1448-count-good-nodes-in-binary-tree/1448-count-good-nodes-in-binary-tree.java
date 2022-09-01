3d0e09ed1357165b82838a30be7194532e411a79
yashikajain18 <95409412+yashikajain18@users.noreply.github.com>
2022-09-01 17:21:16 +0530

    Time: 4 ms (40.75%), Space: 60.1 MB (25.42%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,36 +14,21 @@
  * }
  */
 class Solution {
-    //int count=0;
-    // public int dfs(TreeNode root, int[] max, int[]count){
-    //      if(root==null) return 0;
-    //      max[0]=Math.max(max[0], root.val);
-    //      if(max[0]<=root.val) count[0]++;
-    //      int l= dfs(root.left, max, count);
-    //      int r= dfs(root.right, max, count);
-    //      return count[0];
-    // }
-    // public int goodNodes(TreeNode root) {
-    //   int max[]=new int[1];
-    //   max[0]=root.val;
-    //   int count[]=new int[1];
-    //   count[0]=0;  
-    //   return dfs(root,max, count);
-    // }
-    int count = 0; 
-
+    int count=0;
+    public int dfs(TreeNode root, int max, int[]count){
+         if(root==null) return 0;
+         max=Math.max(max, root.val);
+         if(max<=root.val) count[0]++;
+         int l= dfs(root.left, max, count);
+         int r= dfs(root.right, max, count);
+         return count[0];
+    }
     public int goodNodes(TreeNode root) {
-        helper(root, root.val);
-        return count; 
+       
+    
+      int count[]=new int[1];
+      count[0]=0;  
+      return dfs(root,root.val, count);
     }
 
-    private void helper(TreeNode root, int max) {
-        if (root != null) {
-            if (root.val >= max) { 
-                count++; 
-            }
-            helper(root.left, Math.max(root.val, max));  
-            helper(root.right, Math.max(root.val, max));  
-        }
-    }
 }
