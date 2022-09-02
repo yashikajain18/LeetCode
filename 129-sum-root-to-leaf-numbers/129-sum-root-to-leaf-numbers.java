f241fa61235ccdca8ee7bf0e0e9a6e5d87c6dbd7
yashikajain18 <95409412+yashikajain18@users.noreply.github.com>
2022-09-02 21:41:54 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (22.47%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -13,26 +13,45 @@
  *     }
  * }
  */
- 
+
 class Solution {
     int ans=0;
     
-    private void dfs(TreeNode root, String s){
-        if(root==null) return;
-       
-        if(root.left==null&&root.right==null){
-            
-                ans+=Integer.parseInt(s+root.val,10);
-                return;    
+    private void dfs(TreeNode root, int s){
+        if(root == null) return;
+        
+        s = s*10 + root.val;
+        
+        if(root.left == null && root.right == null){
+            ans += s;
         }
-        s+=root.val;
-        dfs(root.left,s);
-        dfs(root.right,s);
-        return;
+        
+        dfs(root.left, s);
+        dfs(root.right, s);
     }
     public int sumNumbers(TreeNode root) {
        
-        dfs(root, "");
+        dfs(root,0);
         return ans;
     }
 }
+
+// int ans=0;
+    
+//     private void dfs(TreeNode root, String s){
+//         if(root==null) return;
+       
+//         if(root.left==null&&root.right==null){
+//             ans+=Integer.parseInt(s+root.val,10);
+//             return;    
+//         }
+//         s+=root.val;
+//         dfs(root.left,s);
+//         dfs(root.right,s);
+//         return;
+//     }
+//     public int sumNumbers(TreeNode root) {
+       
+//         dfs(root, "");
+//         return ans;
+//     }
