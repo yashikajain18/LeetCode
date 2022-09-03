41d2ce0f76061a91f6df5d6457b3a08bec8d141c
yashikajain18 <95409412+yashikajain18@users.noreply.github.com>
2022-09-03 21:16:31 +0530

    Time: 3 ms (80.94%), Space: 42.9 MB (51.25%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,39 +1,68 @@
 class Solution {
-
-public int[] numsSameConsecDiff(int n, int k) {
-    ArrayList<Integer> ans = new ArrayList<>();
-    
-    for(int i = 1; i <= 9; i++){
-        dfs(n, k,  ans, 0, i);
-    }
-    
-    int[] ret = new int[ans.size()];
-    for(int i = 0; i < ret.length; i++){
-        ret[i] = ans.get(i);
-    }
-    
-    return ret;
-}
-
-public void dfs(int n, int k, ArrayList<Integer> ans, int curr, int digit){
-     if(n < 0) return;
-    
-    if(n == 1){
-        curr = (curr*10)+digit;
-        if(!ans.contains(curr))
-        {
-            ans.add(curr);
+   private void dfs (int num, int digit, int n, int k, List<Integer> numbers) {
+        
+        if (n == 0) {
+            numbers.add (num);
+            return;
         }
-        return;
-    }
-     curr = (curr*10)+digit;
-            
-    if(digit+k >=0 && digit+k <= 9){
-        dfs(n-1, k, ans, curr, digit+k);
+        if (digit + k <= 9) {
+            dfs (num * 10 + (digit + k), digit + k, n - 1, k, numbers);
+        }
+        if (k != 0 && digit - k >= 0) {
+            dfs (num * 10 + (digit - k), digit - k, n - 1, k, numbers);
+        }
     }
     
-    if(digit-k >=0 && digit-k <= 9){
-        dfs(n-1, k, ans, curr, digit-k);
-     }
-   }
+    public int[] numsSameConsecDiff(int n, int k) {
+        
+        List<Integer> numbers = new ArrayList<> ();
+        
+        for (int num = 1; num < 10; num++) {
+            dfs (num, num, n - 1, k, numbers);
+        }
+        
+        int[] answer = new int[numbers.size ()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = numbers.get (i);
+        }
+        
+        return answer;
+    }
+// public int[] numsSameConsecDiff(int n, int k) {
+//     ArrayList<Integer> ans = new ArrayList<>();
+    
+//     for(int i = 1; i <= 9; i++){
+//         dfs(n, k,  ans, 0, i);
+//     }
+    
+//     int[] ret = new int[ans.size()];
+//     for(int i = 0; i < ret.length; i++){
+//         ret[i] = ans.get(i);
+//     }
+    
+//     return ret;
+// }
+
+// public void dfs(int n, int k, ArrayList<Integer> ans, int curr, int digit){
+//      if(n < 0) return;
+    
+//     if(n == 1){
+//         curr = (curr*10)+digit;
+//         if(!ans.contains(curr))
+//         {
+//             ans.add(curr);
+//         }
+//         return;
+//     }
+//      curr = (curr*10)+digit;
+            
+//     if(digit+k >=0 && digit+k <= 9){
+//         dfs(n-1, k, ans, curr, digit+k);
+//     }
+    
+//     if(digit-k >=0 && digit-k <= 9){
+//         dfs(n-1, k, ans, curr, digit-k);
+//      }
+//    }
 }   
